22fc3ec42da05f677e35529a75c58c628bb3747b
csw <csw@qq.com>
2023-04-04 16:52:24 +0800

    add:完结v2版（修改推荐方式为预测评分推荐topN）

--- movie-recommender-by-spark/src/main/scala/UserBased.scala
+++ movie-recommender-by-spark/src/main/scala/UserBased.scala
@@ -40,15 +40,18 @@ object UserBased {
       // 求每个用户的所有rating的平方和开方
       .mapValues(x => sqrt(x.toArray.map(line => pow(line.toDouble, 2)).sum))
       .toDF("user_id_sum", "rating_sqrt_sum")
+
     //定义udf  两列进行相乘
     val productUdf = udf((s1: Int, s2: Int) => s1.toDouble * s2.toDouble)
     // 求同一部电影，不同的两个用户的评分积
     val productDf = joinDf.withColumn("rating_product", productUdf(col("rating"), col("rating2")))
       .select("user_id", "user_id2", "rating_product")
+
     // 求两个用户之间所有共同看过的电影的评分和
     val userGroupDf = productDf.groupBy("user_id", "user_id2")
       .agg("rating_product" -> "sum")
       .withColumnRenamed("sum(rating_product)", "rating_sum_pro")
+
     val userRatingDf = userGroupDf
       .join(broadcast(userSumDf),userGroupDf("user_id")===userSumDf("user_id_sum"))
       .drop("user_id_sum")
@@ -56,7 +59,9 @@ object UserBased {
       .join(broadcast(userSumDf),userGroupDf("user_id2")===userSumDf("user_id_sum"))
       .drop("user_id_sum")
       .withColumnRenamed("rating_sqrt_sum","rating_sqrt_user_id2_sum")
+
     val simUdf = udf((pro:Double,s1:Double,s2:Double)=>pro/(s1*s2))
+
     val simDf = userRatingDf.withColumn("sim",simUdf(col("rating_sum_pro"),col("rating_sqrt_user_id_sum"),col("rating_sqrt_user_id2_sum")))
       .select("user_id","user_id2","sim")
     simDf.show(10) // 两个用户之间的相似度
@@ -68,15 +73,24 @@ object UserBased {
       .option("inferSchema", "true") //推断数据类型
       .csv("hdfs://nameservice1/test/data/movies.csv")
     movieData.describe().show()
+
     movieData.createOrReplaceTempView("movie")
-    val userFilter = simDf.where("user_id = 130").orderBy(desc("sim")).limit(10).select("user_id2","sim")
-    val a = data.join(broadcast(userFilter),userFilter("user_id2") === data("user_id")).select("movie_id","rating","sim")
+    // 限制相似度大于0.6
+    val userFilter = simDf.where("user_id = 130")
+      .where("sim >= 0.6")
+      .orderBy(desc("sim"))
+      .limit(10)
+      .select("user_id2","sim")
+
+    val a = data.join(broadcast(userFilter),userFilter("user_id2") === data("user_id"))
+      .select("movie_id","rating","sim")
+
     a.createOrReplaceTempView("a")
     val movieTop = spark.sql(
       """
         |SELECT /*+ broadcast(b) */
         |       a.movie_id
-        |       ,sum(rating*sim)
+        |       ,sum(rating*sim)/sum(sim)
         |FROM a
         |LEFT JOIN
         |(
@@ -87,10 +101,32 @@ object UserBased {
         |ON a.movie_id = b.movie_id
         |WHERE b.movie_id is null
         |group by a.movie_id
-        |order by sum(rating*sim) desc
+        |order by sum(rating*sim)/sum(sim) desc
         |limit 3
         |""".stripMargin)
-    movieTop.join(movieData,movieData("movie_id") === movieTop("movie_id")).select("title").show(false)
+    // 得出预测评分
+
+    // 为防止预测评分出现单个相似用户评价不具备可靠性，过滤单个用户的movie
+    val movieTop2 = spark.sql(
+      """
+        |SELECT /*+ broadcast(b) */
+        |       a.movie_id
+        |       ,sum(rating*sim)/sum(sim) as rating
+        |FROM a
+        |LEFT JOIN
+        |(
+        |	SELECT  movie_id
+        |	FROM udata
+        |	WHERE user_id = 130
+        |)b
+        |ON a.movie_id = b.movie_id
+        |WHERE b.movie_id is null
+        |group by a.movie_id
+        |having count(*) > 1
+        |""".stripMargin)
+    movieTop2.orderBy(desc("rating")).limit(10).show()
+
+    movieTop2.join(movieData,movieData("movie_id") === movieTop("movie_id")).select("title").show(false)
 
   }
 
